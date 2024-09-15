3ab653a0d85f7d030d286d44213540b70615d620
cohengmc <34006245+cohengmc@users.noreply.github.com>
2024-09-15 14:48:50 +0200

    Done

--- index.js
+++ index.js
@@ -10,8 +10,78 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @param compHex filter for complementary color hex code
  * @returns Promise
  */
-const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+const fetchColors = ({
+  name,
+  hex,
+  compName,
+  compHex
+}) => {
+
+  // Fetch colors from COLORS
+  return fetch(COLORS).then(res => res.json())
+    .then(res => {
+
+      // Upon receiving the colors, return a resolved promise depending on what the input variable is
+
+      return new Promise((resolve, reject) => {
+
+        // Placeholder array to be used to populate for resolve
+        let finalArray = []
+
+
+        // Filtering based on name
+        if (typeof name !== 'undefined') {
+
+          res.forEach(color => {
+            // Compare with names toUpperCase so it will be case insensitive
+            if (color.name.toUpperCase().includes(name.toUpperCase())) {
+              finalArray.push(color)
+            }
+          });
+          resolve(finalArray)
+        }
+
+        // Filtering based on hex
+        if (typeof hex !== 'undefined') {
+
+          res.forEach(color => {
+            if (color.hex === hex) {
+              finalArray.push(color)
+            }
+          });
+          resolve(finalArray)
+        }
+
+        // Filtering based on compName
+        if (typeof compName !== 'undefined') {
+
+          res.forEach(color => {
+            color.comp.forEach(comp => {
+              // Compare with names toUpperCase so it will be case insensitive
+              if (comp.name.toUpperCase().includes(compName.toUpperCase())) {
+                finalArray.push(color)
+              }
+            })
+          });
+          resolve(finalArray)
+        }
+
+        // Filtering based on compHex
+        if (typeof compHex !== 'undefined') {
+
+          res.forEach(color => {
+            color.comp.forEach(comp => {
+              if (comp.hex === compHex) {
+                finalArray.push(color)
+              }
+            })
+          });
+          resolve(finalArray)
+        }
+
+      })
+    })
+
 };
 
 // Leave this here
